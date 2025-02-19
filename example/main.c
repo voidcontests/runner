57620744487abe5dfd745cda973f8c594b6d4c03
jus1d <artfa63@gmail.com>
2025-02-20 01:47:31 +0400

    chore: add example to README.md

--- example/main.c
+++ example/main.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int n, a, b;
-    scanf("%d", &n);
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
-    for (size_t i = 0; i < n; ++i) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a + b);
+void quicksort(int *data, int l, int r) {
+    if (l > r) return;
+
+    int i = l;
+    int j = r;
+    int pivot = data[(l + r) / 2];
+
+    while (i <= j) {
+        while (data[i] < pivot) i++;
+        while (data[j] > pivot) j--;
+
+        if (i <= j) {
+            swap(&data[i], &data[j]);
+
+            i++;
+            j--;
+        }
     }
 
-    return 0;
+    quicksort(data, l, j);
+    quicksort(data, i, r);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* data = (int*) malloc((n) * sizeof(int));
+    if (data == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    quicksort(data, 0, n-1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
 }
